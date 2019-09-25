fad995f0dfc4cd348a83c9f9db3b783603ec2208
zhongcp <1157723258@qq.com>
2019-09-25 18:40:56 +0800

    init

---
+++ kafka-producer/src/main/java/cn/zcp/kafka/SimpleProducer.java
@@ -0,0 +1,19 @@
+package cn.zcp.kafka;
+
+/**
+ * @author zhongchunping
+ * @version 1.0
+ * @Time 2019-09-25 18:23
+ * @describe kafka-parent <描述>
+ */
+public class SimpleProducer {
+
+
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
